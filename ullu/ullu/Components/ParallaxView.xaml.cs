2b5cbc8f2d9c39910c33d6ab7054d139ca5bce7c
gandharv <gandharv.garg@getpromus.com>
2016-03-13 00:46:51 +0530

    Added AddReviewPage & FilterPage

---
+++ ullu/ullu/Components/ParallaxView.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Plugin.ExternalMaps;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace ullu.Components
+{
+    public partial class ParallaxView : ContentView
+    {
+        public ParallaxView()
+        {
+            InitializeComponent();
+            outerScrollView.Scrolled += (sender, e) => Parallax();
+            Parallax();
+        }
+        double _imageHeight;
+        void Parallax()
+        {
+            if (_imageHeight <= 0)
+                _imageHeight = img.Height;
+
+            var y = outerScrollView.ScrollY * .4;
+            if (y >= 0)
+            {
+                //Move the Image's Y coordinate a fraction of the ScrollView's Y position
+                img.Scale = 1;
+                img.TranslationY = y;
+            }
+            else
+            {
+                //Calculate a scale that equalizes the height vs scroll
+                double newHeight = _imageHeight + (outerScrollView.ScrollY * -1);
+                img.Scale = newHeight / _imageHeight;
+                img.TranslationY = outerScrollView.ScrollY / 2;
+            }
+        }
+
+        async void OnMapsButtonClicked(object sender, EventArgs e)
+        {
+            var success = await CrossExternalMaps.Current.NavigateTo("Space Needle", 47.6204, -122.3491);
+        }
+    }
+}
---
+++ ullu/ullu/Views/AddReviewPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace ullu.Views
+{
+    public partial class AddReviewPage : ContentPage
+    {
+        public AddReviewPage()
+        {
+            InitializeComponent();
+            starOne.IsStarred = true;
+        }
+    }
+}
