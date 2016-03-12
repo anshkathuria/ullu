d310b45646f5ef10fe7378040262265e46ac557d
Abhishek Kathuria <abhishek.kathuria@getpromus.com>
2016-03-13 01:42:53 +0530

    Publish to Firebase - Success

---
+++ ullu/ullu/Models/Store.cs
@@ -0,0 +1,23 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ullu.Models
+{
+    [ImplementPropertyChanged]
+    public class Store
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Address { get; set; }
+        public bool IsLocation { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Landmark { get; set; }
+        public string[] Tags { get; set; }
+        
+    }
+}
--- ullu/ullu/Views/AddStorePage.xaml.cs
+++ ullu/ullu/Views/AddStorePage.xaml.cs
@@ -1,7 +1,15 @@
 ﻿using DLToolkit.Forms.Controls;
+using Plugin.Connectivity;
+using Plugin.Geolocator;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using ullu.ActivityIndicator;
 using ullu.Constants;
 using ullu.Models;
+using ullu.Services;
+using ullu.Toasts;
 using ullu.ViewModels;
 using Xamarin.Forms;
 
@@ -50,10 +58,104 @@ namespace ullu.Views
         }
         private async void SaveBtn(object sender, EventArgs e)
         {
-            Button btn = sender as Button;
-            btn.IsEnabled = false;
-            await Navigation.PopAsync();
-            btn.IsEnabled = true;
+            ToolbarItem i  = sender as ToolbarItem;
+            i.Clicked -= SaveBtn;
+            var hud = DependencyService.Get<IHUDProvider>();
+            hud.DisplayProgress("Saving Data");
+            await Task.Delay(500);
+            if (!String.IsNullOrWhiteSpace(nameEntry.Text) && categoryPicker.SelectedIndex >=0 && !String.IsNullOrWhiteSpace(addressEntry.Text) && !String.IsNullOrWhiteSpace(landmarkEntry.Text) && ViewModel.Items.Count > 0)
+            {
+                if (CrossConnectivity.Current.IsConnected)
+                {
+                    FireSharpClient FsClient = FireSharpClient.Instance;
+                    if (IsLocation == false)
+                    {
+                        GoogleMapsService googleMapsService = GoogleMapsService.Instance;
+                        var l = await googleMapsService.GetLocationFromAddress(addressEntry.Text);
+                        if (l != null)
+                        {
+                            CurrentLatitude = l.lat;
+                            CurrentLongitude = l.lng;
+                        }
+                        else
+                        {
+                            var addressInvalid = "Address is not available on Google Maps";
+                            addressInvalid.ToToast(ToastNotificationType.Error);
+                        }
+                    }
+                    Store s = new Store
+                    {
+                        Name = nameEntry.Text,
+                        Category = AppConstants.CATEGORIES[categoryPicker.SelectedIndex],
+                        Address = addressEntry.Text,
+                        IsLocation = this.IsLocation,
+                        Latitude = this.CurrentLatitude,
+                        Longitude = this.CurrentLongitude,
+                        Landmark = landmarkEntry.Text
+                    };
+                    List<string> tempList = new List<string>();
+                    foreach (var item in ViewModel.Items)
+                    {
+                        tempList.Add(item.Name);
+                    }
+                    s.Tags = tempList.ToArray();
+                    var pushKey = await FsClient.Push("stores", s);
+                    hud.Dismiss();
+                    await Navigation.PopAsync();
+                    var msg = "Your data has been saved.";
+                    msg.ToToast(ToastNotificationType.Error);
+                }
+                else
+                {
+                    hud.Dismiss();
+                    var msg = "Make sure you're connected to Internet.";
+                    msg.ToToast(ToastNotificationType.Error);
+                }
+            }
+            else
+            {
+                hud.Dismiss();
+                var msg = "Please Fill All the Fields";
+                msg.ToToast(ToastNotificationType.Error);
+            }
+            hud.Dismiss();
+            i.Clicked += SaveBtn;
+        }
+
+        private bool IsLocation = false;
+        private double CurrentLatitude;
+        private double CurrentLongitude;
+
+        public async void OnGetCurrentLocation(object s, EventArgs e)
+        {
+            if (CrossGeolocator.Current.IsGeolocationEnabled)
+            {
+                try
+                {
+                    var locator = CrossGeolocator.Current;
+                    locator.DesiredAccuracy = 50;
+
+                    var position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
+                    IsLocation = true;
+                    CurrentLatitude = position.Latitude;
+                    CurrentLongitude = position.Longitude;
+                    Debug.WriteLine("Position Status: {0}", position.Timestamp);
+                    Debug.WriteLine("Position Latitude: {0}", CurrentLatitude);
+                    Debug.WriteLine("Position Longitude: {0}", CurrentLongitude);
+                    addressEntry.Text = String.Format("{0}, {1}", CurrentLatitude, CurrentLongitude);
+                }
+                catch (Exception ex)
+                {
+                    var msg = "Unable to get location, may need to increase timeout";
+                    msg.ToToast(ToastNotificationType.Error);
+                }
+            }
+            else
+            {
+                var msg = "Please Turn On Your GPS";
+                msg.ToToast(ToastNotificationType.Error);
+            }
+            
         }
     }
 }
