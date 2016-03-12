89b5255ab7f92472993a72a50c8d307d5062e03f
gandharv <gandharv.garg@getpromus.com>
2016-03-12 15:06:54 +0530

    Added AndHUD loading

---
+++ ullu/ullu.Droid/Activityindicator/HUDProvider.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using System;
+using ullu.ActivityIndicator;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(ullu.Droid.ActivityIndicator.HUDProvider))]
+
+namespace ullu.Droid.Activityindicator
+{
+    public class HUDProvider : IHUDProvider
+    {
+        public void DisplayProgress(string message)
+        {
+            AndroidHUD.AndHUD.Shared.Show(Forms.Context, message);
+        }
+
+        public void DisplaySuccess(string message)
+        {
+            AndroidHUD.AndHUD.Shared.ShowSuccess(Forms.Context, message, AndroidHUD.MaskType.Black, TimeSpan.FromSeconds(1));
+        }
+
+        public void DisplayError(string message)
+        {
+            AndroidHUD.AndHUD.Shared.ShowError(Forms.Context, message, AndroidHUD.MaskType.Black, TimeSpan.FromSeconds(1));
+        }
+
+        public void Dismiss()
+        {
+            AndroidHUD.AndHUD.Shared.Dismiss(Forms.Context);
+        }
+    }
+}
---
+++ ullu/ullu/ActivityIndicator/IHUDProvider.cs
@@ -0,0 +1,13 @@
+﻿namespace ullu.ActivityIndicator
+{
+    public interface IHUDProvider
+    {
+        void DisplayProgress(string message);
+
+        void DisplaySuccess(string message);
+
+        void DisplayError(string message);
+
+        void Dismiss();
+    }
+}
