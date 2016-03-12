6901e530769e459d4f5e00a543c2121e750a346a
gandharv <gandharv.garg@getpromus.com>
2016-03-12 17:07:13 +0530

    Added Parallax Screen

---
+++ ullu/ullu/TestParallaxPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace ullu
+{
+    public partial class TestParallaxPage : ContentPage
+    {
+        public TestParallaxPage()
+        {
+            InitializeComponent();
+            outerScrollView.Scrolled += (sender, e) => Parallax();
+            Parallax();
+        }
+        double _imageHeight;
+        void Parallax()
+        {
+            if (_imageHeight <= 0)
+                _imageHeight = img.Height;
+
+            var y = outerScrollView.ScrollY * .4;
+            if (y >= 0)
+            {
+                //Move the Image's Y coordinate a fraction of the ScrollView's Y position
+                img.Scale = 1;
+                img.TranslationY = y;
+            }
+            else
+            {
+                //Calculate a scale that equalizes the height vs scroll
+                double newHeight = _imageHeight + (outerScrollView.ScrollY * -1);
+                img.Scale = newHeight / _imageHeight;
+                img.TranslationY = outerScrollView.ScrollY / 2;
+            }
+        }
+    }
+}
