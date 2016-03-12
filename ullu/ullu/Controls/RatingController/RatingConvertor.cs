92b30ac058bd955c8b8a22d3af78308e559cb2ef
gandharv <gandharv.garg@getpromus.com>
2016-03-12 16:21:16 +0530

    Added - star behaviour

---
+++ ullu/ullu/Controls/RatingController/RatingConvertor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Controls.RatingController
+{
+    class RatingConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var rating = (int)value;
+            switch (rating)
+            {
+                case 1:
+                    return "Disappointed!";
+                case 2:
+                    return "Not a fan!";
+                case 3:
+                    return "It's Ok!";
+                case 4:
+                    return "Like it!";
+                case 5:
+                    return "Love it!";
+                default:
+                    return string.Empty;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
